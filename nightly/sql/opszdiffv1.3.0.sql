2d8c8ababb8f517896a9844c7c18601358de982a
codecivil <kuehnel@codecivil.de>
2020-06-26 16:59:36 +0200

    SQL corrections

--- nightly/sql/opszdiffv1.3.0.sql
+++ nightly/sql/opszdiffv1.3.0.sql
@@ -8,6 +8,6 @@ UPDATE opsz_aufnahme_permissions SET keyreadable = 'PsychiaterIn (Kontaktdaten)'
 UPDATE opsz_aufnahme_permissions SET keyreadable = 'UnterstützerIn (Kontaktdaten)' WHERE keymachine = 'unterstuetzer';
 UPDATE opsz_aufnahme_permissions SET keyreadable = 'RechtsanwältIn (Kontaktdaten)' WHERE keymachine = 'rechtsanwalt';
 UPDATE opsz_aufnahme_permissions SET keyreadable = 'SWE (UnterstützerIn)' WHERE keymachine = 'swe_unterstuetzer';
-INSERT INTO opsz_termine_permissions (keymachine,keyreadable,realid,typelist,edittype,referencetag) VALUES ('stattgefunden','Termin stattgefunden',4.5,VARCHAR(30),'LIST','_terminstattgefunden');
+INSERT INTO opsz_termine_permissions (keymachine,keyreadable,realid,typelist,edittype,referencetag) VALUES ('stattgefunden','Termin stattgefunden',4.5,'VARCHAR(30)','LIST','_terminstattgefunden');
 INSERT INTO opsz_termine_references (referencetag,allowed_values) VALUES ('_terminstattgefunden','["*ja","Absage KlientIn","Absage PSZ","KlientIn nicht erschienen"]');
 ALTER TABLE opsz_termine ADD COLUMN `stattgefunden` VARCHAR(30);
