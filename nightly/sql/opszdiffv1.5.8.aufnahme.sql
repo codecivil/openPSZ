f5b345fc0fe35a28bd59748c39089781da25f37a
codecivil <codecivil@caspiansea.local>
2021-02-01 18:13:18 +0100

    idempotente SQL für Vermittlungslisten und SWE/Teil2

--- nightly/sql/opszdiffv1.5.8.aufnahme.sql
+++
@@ -1,5 +0,0 @@
--- openStatAdmin-log started 01.31.2021 17:15:26
-ALTER TABLE `opsz_aufnahme` ADD COLUMN `swes` TEXT DEFAULT NULL;
-INSERT  INTO `opsz_aufnahme_permissions` (`keymachine`,`keyreadable`,`realid`,`typelist`,`edittype`,`referencetag`) VALUES ('swes','SWE liegt vor','17.8','VARCHAR(40)','LIST; MULTIPLE','_swes');
-INSERT  INTO `opsz_aufnahme_references` (`referencetag`,`allowed_values`) VALUES ('_swes','["","HausärztIn","PsychiaterIn","UnterstützerIn","RechtsanwältIn","Krankenvers. und Lebensunterhalt","Familienhilfe","Beratungsstellen","PsychotherapeutIn"]');
--- openStatAdmin-log finished 01.31.2021 17:23:41
