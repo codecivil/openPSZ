5ef283a8a55b5c74b550523a9555ca5df3191c84
codecivil <codecivil@caspiansea.local>
2022-02-17 17:19:26 +0100

    technical commit

--- nightly/core/data/info.php
+++
@@ -1,7 +0,0 @@
-<?php
-$author = "codecivil Dr. Marco Kühnel ICT Services";
-$license = "GPLv3";
-$versiondate = "25.1.2022";
-$versionnumber = "1.6.3-opsz-1";
-$contact = "openstat@codecivil.de";
-?>
